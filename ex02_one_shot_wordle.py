fa220d7e30c530d7a26135741fa9bf2b7c18d8a6
milesfoot <112048545+milesfoot@users.noreply.github.com>
2022-09-12 16:14:27 -0400

    Add files via upload

---
+++ ex02_one_shot_wordle.py
@@ -0,0 +1,32 @@
+"""EX01 - One Shot Wordle - Loops """
+
+__author__ = "730613797"
+
+
+SECRET: str = "python"
+word: str = input("What is your 6-letter guess? ")
+i: int = 0 
+guess: bool = False
+guess_boxes: str = ""
+new_counter: int = 0
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+while len(word) != 6:
+        word = input("That was not 6 letters! Try again: ")
+while i < len(SECRET):
+        if word[i] == SECRET[i]: 
+                guess_boxes: str = guess_boxes + GREEN_BOX
+                i += 1
+        else:
+                guess_boxes: str = guess_boxes + WHITE_BOX   
+                i += 1                   
+print(guess_boxes)
+if word == SECRET:
+        print("Woo! You got it!")
+else:
+        print("Not quite. Play again soon!")
+
+
+
